3509bb2b0269c8508544764f827918a9099aeaa1
MofasserHossain <mofasserhossain1221@gmail.com>
2021-02-28 19:24:15 +0600

    file upload

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
-
 const Country = (props) => {
   const { name, capital, flag } = props.country;
+  const [btnDisabled, setBtnDisabled] = useState(false);
   return (
     <div className="country__info">
       <img src={flag} alt={name} />
       <h1>{name}</h1>
       <p>Capital :{capital}</p>
-      <button onClick={() => props.handleAddCountry(props.country)}>
+      <button
+        className={btnDisabled ? 'country__btn disabled' : 'country__btn'}
+        onClick={() => {
+          props.handleAddCountry(props.country);
+          setBtnDisabled(true);
+        }}
+      >
         See Populations
       </button>
-      <button onClick={() => props.handleCountryInfo(props.country)}>
+      <button
+        className="country__btn"
+        onClick={() => props.handleCountryInfo(props.country)}
+      >
         See More Information
       </button>
     </div>
